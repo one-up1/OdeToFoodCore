3a367605233155339fd2130c4c2073e4246926b0
Ferenc Dykstra <oneup1@gmail.com>
2021-03-17 14:04:22 +0100

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Eureka", Location = "Epe", Cuisine = CuisineType.Dutch },
+                new Restaurant { Id = 2, Name = "'t Tonnetje", Location = "Epe", Cuisine = CuisineType.Dutch },
+                new Restaurant { Id = 1, Name = "Domino's Pizza", Location = "Epe", Cuisine = CuisineType.Italian }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
